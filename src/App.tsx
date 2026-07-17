3064dae27f405a04175f84d6c44ea86c2093bd33
Prime0XX00 <1max.thiede@gmail.com>
2026-07-17 17:31:15 +0200

    test boolean in table

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
+import { CheckIcon, XIcon } from "lucide-react";
 import Table from "./table/Table";
 
 type Article = {
 	id: number;
 	data: number;
 	percent: string;
+	active: boolean;
 };
 
 const rows: Article[] = Array.from({ length: 3456 }).map((_, index) => ({
 	id: index,
 	data: Number((100 * Math.random()).toFixed(2)),
 	percent: (100 * Math.random()).toFixed(2),
+	active: index % 3 == 0,
 }));
 
 function App() {
@@ -22,17 +25,20 @@ function App() {
 					{
 						field: "id",
 						title: "ID",
+						dataType: "number",
 						isSortable: true,
 					},
 					{
 						field: "data",
 						title: "Daten",
+						dataType: "number",
 						isSortable: true,
 						isPinned: true,
 					},
 					{
 						field: "percent",
 						title: "Fortschritt",
+						dataType: "string",
 						isSortable: true,
 						initialWidth: 300,
 						render: (value) => (
@@ -41,6 +47,22 @@ function App() {
 									className="absolute left-0 top-0 h-full bg-blue-600"
 									style={{ width: value + "%" }}
 								></div>
+							</div>
+						),
+					},
+					{
+						field: "active",
+						title: "Aktiv",
+						dataType: "boolean",
+						render: (value) => (
+							<div
+								className={`${value ? "bg-green-600/15 text-green-600" : "bg-red-600/15 text-red-600"} p-1 rounded-lg w-fit`}
+							>
+								{value ? (
+									<CheckIcon size={18}></CheckIcon>
+								) : (
+									<XIcon size={18}></XIcon>
+								)}
 							</div>
 						),
 					},
