a9cd41c8aeba44ff62e9220695b38bb71a1a1ab0
Prime0XX00 <1max.thiede@gmail.com>
2026-07-25 16:25:12 +0200

    export popover options

--- src/table/popovers/ExportPopover.tsx
+++ src/table/popovers/ExportPopover.tsx
@@ -1,3 +1,8 @@
+import {
+	FileBracesIcon,
+	FileSpreadsheetIcon,
+	FileTextIcon,
+} from "lucide-react";
 import IconButton from "../IconButton";
 import Popover from "../Popover";
 
@@ -8,7 +13,16 @@ const ExportPopover: React.FC<ExportPopoverProps> = ({ ...props }) => {
 		<Popover trigger={<IconButton icon="download"></IconButton>}>
 			<div className="flex flex-col min-w-20">
 				<div className="h-8.5 flex gap-x-2 px-2 items-center justify-between hover:bg-slate-100 cursor-pointer">
-					JSON
+					<FileTextIcon size={18}></FileTextIcon>
+					<span>CSV</span>
+				</div>
+				<div className="h-8.5 flex gap-x-2 px-2 items-center justify-between hover:bg-slate-100 cursor-pointer">
+					<FileBracesIcon size={18}></FileBracesIcon>
+					<span>JSON</span>
+				</div>
+				<div className="h-8.5 flex gap-x-2 px-2 items-center justify-between hover:bg-slate-100 cursor-pointer">
+					<FileSpreadsheetIcon size={18}></FileSpreadsheetIcon>
+					<span>Excel</span>
 				</div>
 			</div>
 		</Popover>
