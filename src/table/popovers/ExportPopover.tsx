723a7b0b67ec0187781e78bb6c36bc09082ad094
Prime0XX00 <1max.thiede@gmail.com>
2026-08-27 14:49:39 +0200

    cleanup and close on export

--- src/table/popovers/ExportPopover.tsx
+++ src/table/popovers/ExportPopover.tsx
@@ -1,6 +1,6 @@
 import { FileBracesIcon } from "lucide-react";
 import IconButton from "../IconButton";
-import Popover from "../Popover";
+import Popover, { usePopoverClose } from "../Popover";
 import React from "react";
 import Tooltip from "../Tooltip";
 
@@ -8,28 +8,39 @@ interface ExportPopoverProps {
 	exportData: () => void;
 }
 
-const ExportPopover: React.FC<ExportPopoverProps> = ({
-	exportData,
-}: ExportPopoverProps) => {
+function PopoverContent({ ...props }: ExportPopoverProps) {
+	const closePopover = usePopoverClose();
+
+	const handleExportJSON = () => {
+		props.exportData();
+		closePopover?.();
+	};
+
+	return (
+		<div className="flex flex-col min-w-20">
+			<div
+				className="h-8.5 flex gap-x-2 px-2 items-center justify-between hover:bg-main-hover cursor-pointer"
+				onClick={handleExportJSON}
+			>
+				<FileBracesIcon size={18}></FileBracesIcon>
+				<span>JSON</span>
+			</div>
+		</div>
+	);
+}
+
+function ExportPopover({ ...props }: ExportPopoverProps) {
 	return (
 		<Tooltip
 			trigger={
 				<Popover trigger={<IconButton icon="download"></IconButton>}>
-					<div className="flex flex-col min-w-20">
-						<div
-							className="h-8.5 flex gap-x-2 px-2 items-center justify-between hover:bg-main-hover cursor-pointer"
-							onClick={() => exportData()}
-						>
-							<FileBracesIcon size={18}></FileBracesIcon>
-							<span>JSON</span>
-						</div>
-					</div>
+					<PopoverContent {...props}></PopoverContent>
 				</Popover>
 			}
 		>
 			Export
 		</Tooltip>
 	);
-};
+}
 
 export default React.memo(ExportPopover);
