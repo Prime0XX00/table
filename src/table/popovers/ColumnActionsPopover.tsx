46caf37e91a0fb8a45d9a0beb913273f81257f00
Prime0XX00 <1max.thiede@gmail.com>
2026-08-27 14:46:31 +0200

    cleanup column actions

--- src/table/popovers/ColumnActionsPopover.tsx
+++ src/table/popovers/ColumnActionsPopover.tsx
@@ -7,7 +7,7 @@ import {
 } from "lucide-react";
 import type { Column, ColumnState, SortState, TableAction } from "../../types";
 import IconButton from "../IconButton";
-import Popover from "../Popover";
+import Popover, { usePopoverClose } from "../Popover";
 import Tooltip from "../Tooltip";
 
 interface ColumnActionsPopoverProps<RowType> {
@@ -15,6 +15,106 @@ interface ColumnActionsPopoverProps<RowType> {
 	columnState: ColumnState | undefined;
 	sorting: SortState<RowType>;
 	dispatch: (action: TableAction<RowType>) => void;
+}
+
+function PopoverContent<RowType>({
+	...props
+}: ColumnActionsPopoverProps<RowType>) {
+	const closePopover = usePopoverClose();
+
+	const handleTogglePin = () => {
+		props.dispatch({
+			type: "COL_TOGGLE_PIN",
+			payload: {
+				field: props.column.field,
+			},
+		});
+		closePopover?.();
+	};
+
+	const handleSortDesc = () => {
+		props.dispatch({
+			type: "SORT_DESC",
+			payload: {
+				column: props.column,
+			},
+		});
+	};
+
+	const handleSortAsc = () => {
+		props.dispatch({
+			type: "SORT_ASC",
+			payload: {
+				column: props.column,
+			},
+		});
+	};
+
+	const handleSortRemove = () => {
+		props.dispatch({
+			type: "SORT_REMOVE",
+			payload: {
+				column: props.column,
+			},
+		});
+	};
+
+	return (
+		<div className="flex flex-col min-w-20">
+			<div
+				onClick={handleTogglePin}
+				className="h-8.5 flex gap-x-2 px-2 items-center justify-between hover:bg-main-hover cursor-pointer"
+			>
+				{props.columnState?.pinned ? (
+					<>
+						<PinOffIcon size={18}></PinOffIcon>
+						<span>Spalte lösen</span>
+					</>
+				) : (
+					<>
+						<PinIcon size={18}></PinIcon>
+						<span>Spalte fixieren</span>
+					</>
+				)}
+			</div>
+
+			<div className="border-t border-border w-full my-2"></div>
+
+			{((props.sorting.column?.field == props.column.field &&
+				props.sorting.direction == "desc") ||
+				props.sorting.column?.field != props.column.field) && (
+				<div
+					onClick={handleSortAsc}
+					className="h-8.5 flex gap-x-2 px-2 items-center justify-between hover:bg-main-hover cursor-pointer"
+				>
+					<MoveUpIcon size={18}></MoveUpIcon>
+					<span>Aufsteigend sortieren</span>
+				</div>
+			)}
+
+			{((props.sorting.column?.field == props.column.field &&
+				props.sorting.direction == "asc") ||
+				props.sorting.column?.field != props.column.field) && (
+				<div
+					onClick={handleSortDesc}
+					className="h-8.5 flex gap-x-2 px-2 items-center justify-between hover:bg-main-hover cursor-pointer"
+				>
+					<MoveDownIcon size={18}></MoveDownIcon>
+					<span>Absteigend sortieren</span>
+				</div>
+			)}
+
+			{props.sorting.column?.field == props.column.field && (
+				<div
+					onClick={handleSortRemove}
+					className="h-8.5 flex gap-x-2 px-2 items-center justify-between hover:bg-main-hover cursor-pointer"
+				>
+					<MinusIcon size={18}></MinusIcon>
+					<span>Sortierung entfernen</span>
+				</div>
+			)}
+		</div>
+	);
 }
 
 function ColumnActionsPopover<RowType>({
@@ -32,114 +132,7 @@ function ColumnActionsPopover<RowType>({
 						></IconButton>
 					}
 				>
-					<div className="flex flex-col min-w-20">
-						<div
-							onClick={() =>
-								props.dispatch({
-									type: "COL_TOGGLE_PIN",
-									payload: {
-										field: props.column.field,
-									},
-								})
-							}
-							className="h-8.5 flex gap-x-2 px-2 items-center justify-between hover:bg-main-hover cursor-pointer"
-						>
-							{props.columnState?.pinned ? (
-								<>
-									<PinOffIcon size={18}></PinOffIcon>
-									<span>Spalte lösen</span>
-								</>
-							) : (
-								<>
-									<PinIcon size={18}></PinIcon>
-									<span>Spalte fixieren</span>
-								</>
-							)}
-						</div>
-
-						<div className="border-t border-border w-full my-2"></div>
-
-						{props.sorting.column?.field == props.column.field ? (
-							<>
-								{props.sorting.direction == "asc" ? (
-									<div
-										onClick={() =>
-											props.dispatch({
-												type: "SORT_DESC",
-												payload: {
-													column: props.column,
-												},
-											})
-										}
-										className="h-8.5 flex gap-x-2 px-2 items-center justify-between hover:bg-main-hover cursor-pointer"
-									>
-										<MoveDownIcon size={18}></MoveDownIcon>
-										<span>Absteigend sortieren</span>
-									</div>
-								) : (
-									<div
-										onClick={() =>
-											props.dispatch({
-												type: "SORT_ASC",
-												payload: {
-													column: props.column,
-												},
-											})
-										}
-										className="h-8.5 flex gap-x-2 px-2 items-center justify-between bg-main-hover cursor-pointer"
-									>
-										<MoveUpIcon size={18}></MoveUpIcon>
-										<span>Aufsteigend sortieren</span>
-									</div>
-								)}
-								<div
-									onClick={() =>
-										props.dispatch({
-											type: "SORT_REMOVE",
-											payload: {
-												column: props.column,
-											},
-										})
-									}
-									className="h-8.5 flex gap-x-2 px-2 items-center justify-between hover:bg-main-hover cursor-pointer"
-								>
-									<MinusIcon size={18}></MinusIcon>
-									<span>Sortierung entfernen</span>
-								</div>
-							</>
-						) : (
-							<>
-								<div
-									onClick={() =>
-										props.dispatch({
-											type: "SORT_ASC",
-											payload: {
-												column: props.column,
-											},
-										})
-									}
-									className="h-8.5 flex gap-x-2 px-2 items-center justify-between hover:bg-main-hover cursor-pointer"
-								>
-									<MoveUpIcon size={18}></MoveUpIcon>
-									<span>Aufsteigend sortieren</span>
-								</div>
-								<div
-									onClick={() =>
-										props.dispatch({
-											type: "SORT_DESC",
-											payload: {
-												column: props.column,
-											},
-										})
-									}
-									className="h-8.5 flex gap-x-2 px-2 items-center justify-between hover:bg-main-hover cursor-pointer"
-								>
-									<MoveDownIcon size={18}></MoveDownIcon>
-									<span>Absteigend sortieren</span>
-								</div>
-							</>
-						)}
-					</div>
+					<PopoverContent {...props}></PopoverContent>
 				</Popover>
 			}
 		>
